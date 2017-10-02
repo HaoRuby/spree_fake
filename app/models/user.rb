62e3c0ddb65214dd91ad1db46d63778516f32b4b
HaoNguyen <haont@hoatamus.com>
2017-10-02 19:28:13 +0700

    Roling user to admin and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  petergate(roles: [:admin], multiple: false)
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get '/', to: 'pages#index'
+  end
+
   devise_for :users, path: '', path_names: { sign_in: 'login',
                                              sign_out: 'logout',
                                              sign_up: 'register' }
